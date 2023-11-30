50fdd6f84832240774a86c5ffe29bd613e09ccd6
alexandra <alexsafonova1092@gmail.com>
2023-11-30 18:29:33 +0500

    It's my test

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,14 +7,6 @@ import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userServiceImpl = new UserServiceImpl();
-        userServiceImpl.createUsersTable();
-        userServiceImpl.saveUser("Ivan", "Ivanov", (byte) 25);
-        userServiceImpl.saveUser("Petr", "Petrov", (byte) 35);
-        userServiceImpl.saveUser("Sidor", "Sidorov", (byte) 45);
-        userServiceImpl.saveUser("Alexey", "Alexeev", (byte) 55);
-        System.out.println(userServiceImpl.getAllUsers());
-        userServiceImpl.cleanUsersTable();
-        userServiceImpl.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,19 +4,9 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Util {
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/Users";
-    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_USERNAME = "root";
-    private static final String DB_PASSWORD = "rootroot";
+
     public Connection getConnection() {
-        Connection connection = null;
-        try {
-            Class.forName(DB_DRIVER);
-            connection = java.sql.DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-            System.out.println("Успешное подключение к базе данных");
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-        return connection;
+
+        return null;
     }
 }
